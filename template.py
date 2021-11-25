791b9ab27370c2e44d1a981dafc157ab619e7c55
DannyWeston <danielweston37@gmail.com>
2021-11-25 23:39:39 +0000

    Updated template file

--- template.py
+++ template.py
@@ -1,1 +1,14 @@
-#template
+#!/usr/bin/python
+
+# DannyWeston's Solutions for the Project Euler website
+# Problem (#)
+
+import sys
+
+# argc = Argument count
+# argv = Argument values
+def main(argc, argv):
+    pass
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
