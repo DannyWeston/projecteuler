1ee182ff1ff43f0e52f8b7ef573613c7a1116a1b
DannyWeston <danielweston37@gmail.com>
2021-12-18 02:24:01 +0000

    Completed

---
+++ problem18.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# DannyWeston's Solutions for the Project Euler website
+# Problem (#)
+
+import sys
+
+triangle = [
+    [75],
+    [95, 64],
+    [17, 47, 82],
+    [18, 35, 87, 10],
+    [20, 4, 82, 47, 65],
+    [19, 1, 23, 75, 3, 34],
+    [88, 2, 77, 73, 7, 63, 67],
+    [99, 65, 4, 28, 6, 16, 70, 92],
+    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+]
+
+highest = 0
+
+# argc = Argument count
+# argv = Argument values
+def main(argc, argv):
+    explore(0, 0, 0, triangle)
+
+    print(highest)
+
+
+def explore(row, col, total, triangle):
+    global highest
+
+    # Terminate if nothing else to expand
+    if row == len(triangle):
+        if highest < total: highest = total
+        return
+
+    newTotal = total + triangle[row][col]
+
+    # left
+    explore(row + 1, col, newTotal, triangle)
+
+    # right
+    explore(row + 1, col + 1, newTotal, triangle)
+
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
